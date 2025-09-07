f7172cd4ac5fa9fe4947997244148dc4f2a140a4
kirill-kolomin <kkolomin.w@gmail.com>
2025-09-07 06:37:49 +0300

    Change titles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            NgxTestbox Tutorial - 10min ⏱️
+            NgxTestbox Tutorial - 5min ⏱️
           </Link>
         </div>
       </div>
